2e937a9fd281ce6cf0693763ddb5c4e1e0d316b8
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-13 20:44:48 +0700

    show user

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AppState, User } from './types';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="container">
+      <app-sign-in></app-sign-in>
+      <app-profile></app-profile>
+      <pre>{{ user | json }}</pre>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: User;
+
+  constructor(private store: Store<AppState>) {
+    this.store.select('user').subscribe(u => this.user = u);
+  }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../types';
 import { RequestService } from './request.service';
 
 @Injectable()
 
 export class UserService {
-    constructor(private request: RequestService) {}
+    constructor(private request: RequestService, private store: Store<AppState>) {}
 
     signIn(email: string, password: string) {
         this.request.post('/user/signin', { email, password })
-        .then(response => alert(JSON.stringify(response)))
-        .catch(error => alert(JSON.stringify(error)));
+        .then(response => {
+            this.store.dispatch({ type: 'SET_USER', user: response.user });
+        })
+        .catch(error => alert(error.message));
     }
 }
