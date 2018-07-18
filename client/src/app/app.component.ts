a1585fb0fbf8095d9cd176996a04e908f6f4790d
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-18 20:20:48 +0700

    show all stories

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { AppState, User, Story } from './types';
+import { AppState, User } from './types';
 import { Store } from '@ngrx/store';
 import { UserService } from './services/user.service';
-import { StoryService } from './services/story.service';
 
 @Component({
   selector: 'app-root',
@@ -12,26 +11,19 @@ import { StoryService } from './services/story.service';
       <router-outlet></router-outlet>
     </div>
     <div class="container" *ngIf="!loaded">Loading...</div>
-    <div class="container">
-      <br>
-      <pre>{{ stories | json }}</pre>
-    </div>
   `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   user: User;
-  stories: Story[];
   loaded: boolean;
   constructor(
     private userSerivce: UserService,
     private store: Store<AppState>,
-    private storyService: StoryService
   ) {
     this.store.select('user').subscribe(u => this.user = u);
     this.store.select('loaded').subscribe(l => this.loaded = l);
-    this.store.select('stories').subscribe(l => this.stories = l);
     this.userSerivce.check();
-    this.storyService.getAllStories();
   }
 }
--- client/src/app/components/home.component.ts
+++ client/src/app/components/home.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { AppState, User, Story } from '../types';
+import { Store } from '@ngrx/store';
+import { StoryService } from '../services/story.service';
 
 @Component({
     selector: 'app-home',
     template: `
         <h4>Home component</h4>
+        <div *ngIf="!user">
+            Gioi thieu gi do o day.
+            <a routerLink="/signup">Dang ky tai day.</a>
+        </div>
+        <div *ngIf="user">
+            <pre>
+                {{ stories | json }}
+            </pre>
+        </div>
     `
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    stories: Story[];
+    user: User;
+    constructor(
+        private store: Store<AppState>,
+        private storyService: StoryService
+    ) {
+        this.store.select('user').subscribe(u => this.user = u);
+        this.store.select('stories').subscribe(l => this.stories = l);
+        this.storyService.getAllStories();
+    }
+}
