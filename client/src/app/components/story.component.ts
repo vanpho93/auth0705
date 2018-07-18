f609d49711ecf035261cc56f42a66e8d48d96dca
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-18 20:47:31 +0700

    css

--- client/src/app/components/story.component.ts
+++ client/src/app/components/story.component.ts
@@ -4,7 +4,26 @@ import { StoryService } from '../services/story.service';
 
 @Component({
     selector: 'app-story',
-    templateUrl: './story.component.html'
+    templateUrl: './story.component.html',
+    styles: [`
+        .story {
+            margin: 15px;
+            padding: 10px;
+            background-color: #ECEAEC;
+            border-radius: 5px;
+        }
+        .story img {
+            width: 50px;
+            height: 50px;
+            border-radius: 50px;
+            display: inline-block;
+        }
+        h5 { float: right; }
+        .header {
+            width: 150px;
+            margin-bottom: 10px;
+        }
+    `]
 })
 
 export class StoryComponent {
