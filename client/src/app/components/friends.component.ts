4990ac5d38ae96922d4e7d4f27098035e936956d
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-23 20:45:54 +0700

    ui

--- client/src/app/components/friends.component.ts
+++ client/src/app/components/friends.component.ts
@@ -7,8 +7,34 @@ import { AppState, People } from '../types';
     selector: 'app-friends',
     template: `
         <h4>Friends component</h4>
-        <pre>{{ people | json }}</pre>
-    `
+        <div class="user">
+            <div class="header">
+                <img src="https://s3.amazonaws.com/uifaces/faces/twitter/antonyzotov/128.jpg" />
+                <h4>Abcd</h4>
+            </div>
+            <button class="btn btn-success">Add Friend</button>
+            <button class="btn btn-success">Remove Friend</button>
+        </div>
+    `,
+    styles: [`
+        .header h4 { float: left; margin-left: 10px; }
+        .header img{float: left;}
+        div.header { width: 200px; height: 100px; margin-bottom: 10px; }
+        .btn-success{
+            color: #fff;
+            background-color: #449d44;
+            border-color: #398439;
+            margin-top: -94px;
+            margin-left: 10px;
+        }
+
+        .user {
+            background-color: #E9E9E9;
+            margin: 10px;
+            padding: 10px;
+            border-radius: 10px;
+        }
+    `]
 })
 
 export class FriendsComponent {
