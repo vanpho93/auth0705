0e0e7b48002ac58cceea60dc8f15497b0eb0d96e
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-25 21:13:09 +0700

    xong

--- client/src/app/components/avatar.component.ts
+++ client/src/app/components/avatar.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'app-avatar',
     template: `
-        <h4>Avatar component</h4>
-    `
+        <input type="file" (change)="onFileChanged($event)"/>
+        <pre>{{ file?.name }}</pre>
+        <button (click)="upload();">Upload</button>
+        `
 })
 
-export class AvatarComponent {}
+export class AvatarComponent {
+    file: File;
+
+    constructor(private http: Http) {}
+
+    onFileChanged(event) {
+        this.file = event.target.files[0];
+        console.log(this.file);
+    }
+
+    upload() {
+        const formData = new FormData();
+        formData.append('image', this.file, this.file.name);
+        this.http.post('http://localhost:3000/avatar', formData)
+        .toPromise()
+        .then(() => alert('Upload thanh cong'))
+        .catch((error) => console.log(error));
+    }
+}
